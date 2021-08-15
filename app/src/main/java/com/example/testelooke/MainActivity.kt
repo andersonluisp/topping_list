e2f997fae4c87419e972446bd85b63baf6c5bf69
andersonluisp <andersonluiscp@gmail.com>
2021-08-15 10:49:28 -0300

    Refactoring with Ktlint

--- app/src/main/java/com/example/testelooke/MainActivity.kt
+++ app/src/main/java/com/example/testelooke/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.testelooke
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.testelooke.ui.main.fragment.MainFragment
 
 class MainActivity : AppCompatActivity() {
--- app/src/main/java/com/example/testelooke/ui/main/fragment/MainFragment.kt
+++ app/src/main/java/com/example/testelooke/ui/main/fragment/MainFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.testelooke.R
 import com.example.testelooke.app.gone
@@ -16,14 +17,15 @@ import com.example.testelooke.ui.main.viewmodel.MainViewModel
 class MainFragment : Fragment() {
 
     private lateinit var b: MainFragmentBinding
-    private val vm by lazy { MainViewModel() }
+    private val vm by viewModels<MainViewModel>()
 
     companion object {
         fun newInstance() = MainFragment()
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         b = MainFragmentBinding.inflate(inflater)
